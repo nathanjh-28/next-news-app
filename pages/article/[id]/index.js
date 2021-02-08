cb911bedf3a787bcf88a16d6a1a990315606aa81
nathanjh-28 <nathan@nathanjharris.com>
2021-02-08 12:06:17 -0700

    made a little rest api inside project

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from '../../../config'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
@@ -13,7 +14,7 @@ const article = ({ article }) => {
 }
 
 export const getStaticProps = async (context) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
     const article = await res.json()
     return {
         props: {
@@ -22,8 +23,18 @@ export const getStaticProps = async (context) => {
     }
 }
 
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//     const article = await res.json()
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
 export const getStaticPaths = async () =>{
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+    const res = await fetch(`${server}/api/articles`)
     const articles = await res.json()
     const ids = articles.map(article => article.id)
     const paths = ids.map(id=>({params: {id:id.toString()}}))
