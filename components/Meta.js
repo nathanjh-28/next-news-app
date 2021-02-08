d2bd7aa8a6bc2839065ab0b8a5d94af4a041e067
nathanjh-28 <nathan@nathanjharris.com>
2021-02-08 12:55:57 -0700

    messing with meta tags

---
+++ components/Meta.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Meta = ({ title, keywords, description }) => {
+    return (
+        <Head>
+            <div>hello</div>
+            
+        </Head>
+    )
+}
+
+export default Meta
